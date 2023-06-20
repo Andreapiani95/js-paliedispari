08bd77806097da1f72ac5d0392402e74b199ab10
Andreapiani95 <andreapiani1995@gmail.com>
2023-06-20 16:51:40 +0200

    loop for, condition set

--- palindroma/js/scripts.js
+++ palindroma/js/scripts.js
@@ -1,4 +1,37 @@
 const wordFromPrompt = prompt ('Inserisci una parola');
-    function isPalindrome (wordFromPrompt){
+console.log(wordFromPrompt);
+
+//1.
+const lettersFromPrompt = wordFromPrompt.split("");
+    console.log(lettersFromPrompt);
+
+//2.
+const lettersFromPromptBackwards = lettersFromPrompt.reverse();
+    console.log(lettersFromPromptBackwards);
+
+    for (i = 0; i < lettersFromPrompt.length; i++){
         
+       if(lettersFromPrompt[i] == lettersFromPromptBackwards[i]){
+            document.getElementById('output').innertext = "La parola è palindroma";
+       }
+       else {
+            document.getElementById('output').innerhtml = "La parola NON è palindroma";
+       }
+       console.log(lettersFromPrompt[i], lettersFromPromptBackwards[i]);
+    
+        
+    };
+
+
+
+
+
+
+    function isPalindrome (){
+    // Trasformo la parola in un array
+    // Inverto l'ordine
+    // Controllo che ciascun index dei 2 array sia uguale con un loop
+    
+        
+
     }
