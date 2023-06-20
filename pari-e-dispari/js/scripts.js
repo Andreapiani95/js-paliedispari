eca9c049fa2b18545ad23ef04ece6409aececdcb
Andreapiani95 <andreapiani1995@gmail.com>
2023-06-20 18:16:45 +0200

    Pari e dispari Complete

--- pari-e-dispari/js/scripts.js
+++ pari-e-dispari/js/scripts.js
@@ -1,33 +1,35 @@
 const resultFromPrompt = prompt('Scegli tra pari o dispari');
-
-const numberFromPrompt = prompt ('Inserisci un numero tra 1 e 5');
-
-function evenOrOdd(inputResult, inputNumber){
-    if (inputResult = 'pari'){
+console.log(resultFromPrompt);
+    if (resultFromPrompt == 'pari'){
         let flagResult = 0;
     }
-    else if (inputResult = 'dispari'){
+    else if (resultFromPrompt == 'dispari'){
         let flagResult = 1;
     }
     else{
-        alert('inserisci pari oppure dispari')
+        alert ('"pari" o "dispari"');
     }
 
+const numberFromPrompt = prompt ('Inserisci un numero tra 1 e 5');
+console.log(numberFromPrompt);
+
+function evenOrOdd (inputResult, inputNumber){
     let computerNumber = Math.floor(Math.random()*5)+1;
-    let sum = computerNumber + inputNumber;
+    console.log(computerNumber);
+    let sum = inputNumber + computerNumber;
+    console.log(sum);
 
     if (sum % 2 == 0){
-        let flagSum = 0;
+        return 0;
     }
     else{
-        let flagSum = 1;
+        return 1;
     }
+}
 
-    if (flagResult == flagSum){
-        document.getElementById('output').innerHTML = 'Evviva, hai vinto!';
-    }
-    else{
-        document.getElementById('output').innerHTML = 'Peccato hai perso!';
-    }
-
+if (flagResult = evenOrOdd){
+    document.getElementById('output').innerHTML = 'Evviva, hai vinto!';
+}
+else{
+    document.getElementById('output').innerHTML = 'Peccato, hai perso!';
 }
