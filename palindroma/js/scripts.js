7e236fcf625950c5ece77cbb54e3da29b6f98e7a
Andreapiani95 <andreapiani1995@gmail.com>
2023-06-20 17:11:42 +0200

    Palindroma 1 complete

--- palindroma/js/scripts.js
+++ palindroma/js/scripts.js
@@ -1,37 +1,41 @@
 const wordFromPrompt = prompt ('Inserisci una parola');
 console.log(wordFromPrompt);
 
-//1.
-const lettersFromPrompt = wordFromPrompt.split("");
-    console.log(lettersFromPrompt);
 
-//2.
-const lettersFromPromptBackwards = lettersFromPrompt.reverse();
-    console.log(lettersFromPromptBackwards);
-
-    for (i = 0; i < lettersFromPrompt.length; i++){
-        
-       if(lettersFromPrompt[i] == lettersFromPromptBackwards[i]){
-            document.getElementById('output').innertext = "La parola è palindroma";
-       }
-       else {
-            document.getElementById('output').innerhtml = "La parola NON è palindroma";
-       }
-       console.log(lettersFromPrompt[i], lettersFromPromptBackwards[i]);
-    
-        
-    };
-
-
-
-
-
-
-    function isPalindrome (){
+function isPalindrome(inputWord){
     // Trasformo la parola in un array
     // Inverto l'ordine
     // Controllo che ciascun index dei 2 array sia uguale con un loop
-    
+    const lettersFromPrompt = inputWord.split("");
+    console.log(lettersFromPrompt);
+
+    const lettersFromPromptBackwards = lettersFromPrompt.reverse();
+    console.log(lettersFromPrompt);
+
+    /*
+    const inputWordBackwards = lettersFromPromptBackwards.join("");
+
+    if(inputWordBackwards == inputWord){        
+        document.getElementById('output').innerText = "La parola è palindroma";
+    }
+    else {
+        document.getElementById('output').innerText = "La parola NON è palindroma";
+    }
+    */
+
+    for (i = 0; i < lettersFromPrompt.length; i++){
+        
+        if(lettersFromPrompt[i] == lettersFromPromptBackwards[i]){
+             document.getElementById('output').innerText = "La parola è palindroma";
+            }
+        else {
+             document.getElementById('output').innerText = "La parola NON è palindroma";
+        }
+        console.log(i,lettersFromPrompt[i], lettersFromPromptBackwards[i]);
+         
+     };
         
 
-    }
+}
+
+isPalindrome(wordFromPrompt);
